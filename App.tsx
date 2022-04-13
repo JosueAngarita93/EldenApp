de8104089a33397cf20c50c7847f48d4de7f06a4
Josue Angarita <josue.angarita@devbase.us>
2022-04-12 21:57:24 -0500

    Tab de navegacion

--- App.tsx
+++ App.tsx
@@ -1,24 +1,87 @@
 import React from 'react';
 import SplashScreen from 'react-native-splash-screen';
+import {Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 import Home from './src/screens/home';
 import Details from './src/screens/details';
 import Lingrave from './src/screens/lingrave';
+function Profile() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Profile!</Text>
+    </View>
+  );
+}
+
+function Notifications() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Notifications!</Text>
+    </View>
+  );
+}
 const Stack = createNativeStackNavigator();
-const App = () => {
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Feed"
+      screenOptions={{
+        tabBarActiveTintColor: '#e91e63',
+      }}>
+      <Tab.Screen
+        name="Feed"
+        component={Home}
+        options={{
+          tabBarLabel: 'Home',
+          headerShown: false,
+          tabBarIcon: ({color, size}) => (
+            <Icon name="rocket" size={30} color="#900" />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={Notifications}
+        options={{
+          tabBarLabel: 'Updates',
+          headerShown: false,
+          tabBarIcon: ({color, size}) => (
+            <Icon name="rocket" size={30} color="#900" />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarLabel: 'Profile',
+          headerShown: false,
+          tabBarIcon: ({color, size}) => (
+            <Icon name="rocket" size={30} color="#900" />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+export default function App() {
   React.useEffect(() => {
     SplashScreen.hide();
   }, []);
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={MyTabs} />
         <Stack.Screen name="Details" component={Details} />
         <Stack.Screen name="Lingrave" component={Lingrave} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-};
-
-export default App;
+}
